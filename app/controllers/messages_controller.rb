3c4986fe90c67227d257cec6252109e3af5c6588
Dimitri Almon <dimitri.almon@gmail.com>
2024-11-10 23:07:30 +0100

    Adding channel messages file upload

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,15 @@ class MessagesController < ApplicationController
   before_action :authenticate_user_from_token!
   before_action :set_channel_and_discussion
 
+  def index
+    @messages = @discussion.messages.order(created_at: :asc)
+    render json: @messages, status: :ok
+  end
+
   def create
     @message = @discussion.messages.new(message_params)
     @message.user = current_user
-    @message.channel_id = @channel.id # Ajout explicite du channel_id
+    @message.channel_id = @channel.id
 
     if @message.save
       render json: @message, status: :created
@@ -14,16 +19,30 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    @message = @discussion.messages.find_by(id: params[:id])
+    if @message
+      if @message.destroy
+        render json: { message: 'Message deleted successfully' }, status: :ok
+      else
+        render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { errors: ['Message not found'] }, status: :not_found
+    end
+  end
+
   private
 
   def set_channel_and_discussion
-    @channel = Channel.find(params[:channel_id])
-    @discussion = @channel.discussions.find(params[:discussion_id])
-  rescue ActiveRecord::RecordNotFound
-    render json: { errors: ['Channel or Discussion not found'] }, status: :not_found
+    @channel = Channel.find_by(id: params[:channel_id])
+    @discussion = @channel&.discussions.find_by(id: params[:discussion_id])
+    unless @channel && @discussion
+      render json: { errors: ['Channel or Discussion not found'] }, status: :not_found
+    end
   end
 
   def message_params
-    params.require(:message).permit(:content, :file_url)
+    params.require(:message).permit(:content, :file_url, :file)
   end
 end
